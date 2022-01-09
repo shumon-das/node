09909c8649e322607b496418a8acde342678106b
monoranjan.das <monoranjan.das@covianalytics.com>
2022-01-09 18:57:50 +0600

    Checks page complete

---
+++ appHelpers/notifications.js
@@ -0,0 +1,7 @@
+/**
+ * Title: Notifications Library
+ * Description:
+ * Author: monoranjan
+ * Date: 21/11/2021
+ */
+// dependencies
--- handlers/routeHandlers/checkHandler.js
+++ handlers/routeHandlers/checkHandler.js
@@ -92,7 +92,7 @@
                                             userObject.checks = userChecks;
                                             userObject.checks.push(checkId);
                                             // lets save new userdata
-                                            reqeustData.create('users', username, userObject, (userDataSaveError) => {
+                                            reqeustData.update('users', username, userObject, (userDataSaveError) => {
                                                 if(!userDataSaveError){
                                                     callback(200, checkObject);
                                                 }else{
@@ -103,7 +103,7 @@
                                             }) 
                                         }else{
                                             callback(500, {
-                                                error: 'Internal server error'
+                                                error: 'Internal server error '+saveError
                                             })
                                         }
                                     })
@@ -138,15 +138,213 @@
 }
  
  handler._check.get = (requestProperties, callback) => {
-
+    const id = typeof(requestProperties.queryStringObject.id) === 'string' &&
+                    requestProperties.queryStringObject.id.trim()
+                    ? requestProperties.queryStringObject.id 
+                    : false; 
+    if(id){
+        reqeustData.read('Checks', id, (idError, checksData) => {
+            if(!idError && checksData){
+                const token = typeof requestProperties.headers.token === 'string' 
+                              ? requestProperties.headers.token
+                              : false;
+                let username = parseJSON(checksData).username              
+                tokenHandler._token.verify(token, username, (tokenIsValid) => {
+                    if(tokenIsValid){  
+                        callback(200, parseJSON(checksData))
+                    }else{
+                        callback(403, {
+                            error: "Authentication failed"
+                        })
+                    }
+                })     
+            }else{
+                callback(500, {
+                    error: "Internel server error "+ idError
+                })
+            }
+        })
+    }else{
+        callback(400, {
+            error: "TokenError "+id
+        })
+    }
  }
  
  handler._check.put = (requestProperties, callback) => {
-    
+
+    const id = typeof requestProperties.body.id === 'string' &&
+               requestProperties.body.id.trim()
+               ? requestProperties.body.id 
+               : false; 
+
+    let protocol = typeof requestProperties.body.protocol === 'string' &&
+                   ['http', 'https'].indexOf(requestProperties.body.protocol) > -1 
+                   ? requestProperties.body.protocol
+                   : false;
+
+    let url = typeof(requestProperties.body.url) === 'string' &&
+               requestProperties.body.url.trim().length > 0 
+               ? requestProperties.body.url
+               : false; 
+
+    let method = typeof(requestProperties.body.method) === 'string' &&
+                 ['GET','POST','PUT','DELETE'].indexOf(requestProperties.body.method) > -1 
+                 ? requestProperties.body.method
+                 : false;
+
+    let successCodes = typeof(requestProperties.body.successCodes) === 'object' &&
+                       requestProperties.body.successCodes instanceof Array 
+                       ? requestProperties.body.successCodes
+                       : false; 
+
+    let timeoutSeconds = typeof(requestProperties.body.timeoutSeconds) === 'number' &&
+                         requestProperties.body.timeoutSeconds % 1 === 0 &&
+                         requestProperties.body.timeoutSeconds >= 1 &&
+                         requestProperties.body.timeoutSeconds <= 5  
+                         ? requestProperties.body.timeoutSeconds
+                         : false;   
+                      
+    if(id){
+        if(protocol || url || method || successCodes || timeoutSeconds){
+            reqeustData.read('Checks', id, (idError, checksData) => {
+                if(!idError && checksData){
+                    const token = typeof requestProperties.headers.token === 'string' 
+                                  ? requestProperties.headers.token
+                                  : false;
+
+                    let checksDataObject = parseJSON(checksData);  
+
+                    tokenHandler._token.verify(token, checksDataObject.username, (tokenIsValid) => {
+                        if(tokenIsValid){ 
+                            if(protocol){
+                                checksDataObject.protocol = protocol
+                            }
+                            if(url){
+                                checksDataObject.url = url
+                            }
+                            if(method){
+                                checksDataObject.method = method                                
+                            }
+                            if(successCodes){
+                                checksDataObject.successCodes = successCodes
+                            }
+                            if(timeoutSeconds){
+                                checksDataObject.timeoutSeconds = timeoutSeconds
+                            }
+
+                            // save update data
+                            reqeustData.update('Checks', id, checksDataObject, (updateError) => {
+                                if(!updateError){
+                                    callback(200, checksDataObject)
+                                }else{
+                                    callback(500, {
+                                        error: "Internel server error"
+                                    })
+                                }
+                            })
+                        }else{
+                            callback(403, {
+                                error: "Authentication failed"
+                            })
+                        }
+                    })
+                }else{
+                    callback(400,{
+                        error: "ID Error "+ idError
+                    })
+                }
+            })
+        }else{
+            callback(400,{
+                error: "atleast 1 field required to do update"
+            })
+        }
+    }else{
+        callback(400,{
+            error: "Request Error "+ id
+        });
+    }      
  }
  
  handler._check.delete = (requestProperties, callback) => {
+    const id = typeof(requestProperties.queryStringObject.id) === 'string' &&
+                    requestProperties.queryStringObject.id.trim()
+                    ? requestProperties.queryStringObject.id 
+                    : false; 
+    if(id){
+        reqeustData.read('Checks', id, (idError, checksData) => {
+            if(!idError && checksData){
+                const token = typeof requestProperties.headers.token === 'string' 
+                              ? requestProperties.headers.token
+                              : false;
+                let username = parseJSON(checksData).username              
+                tokenHandler._token.verify(token, username, (tokenIsValid) => {
+                    
+                    if(tokenIsValid){  
+                        
+                        reqeustData.delete('Checks', id, (deleteError) => {
+                            
+                            if(!deleteError){
+                                
+                                reqeustData.read('users', username, (userNotFoundError, userData) => {
+                                    let userObject = parseJSON(userData)
+                                    if(!userNotFoundError && userData){
+                                        let userChecks = typeof(userObject.checks) === 'object' &&
+                                                         userObject.checks instanceof Array
+                                                         ? userObject.checks
+                                                         : [];
 
+                                        let checkPosition = userChecks.indexOf(id)
+                                        if(checkPosition > -1){
+                                            userChecks.splice(checkPosition, 1)
+
+                                            // save again the updated data
+                                            userObject.checks = userChecks
+                                            console.log(userObject.username)
+                                            reqeustData.update('users', username, userObject, (userChecksUpdateError) => {
+                                                if(!userChecksUpdateError){
+                                                    callback(200)
+                                                }else{
+                                                    callback(500, {
+                                                        error: "Internel server error " + userChecksUpdateError
+                                                    })
+                                                }
+                                            })
+                                        }else{
+                                            callback(500, {
+                                                error: "id not indexed OR your requested id not found " + checkPosition
+                                            })
+                                        }                 
+                                    }else{
+                                        callback(500, {
+                                            error: "Requested user not found " + userNotFoundError
+                                        })
+                                    }
+                                })
+                            }else{
+                                callback(500, {
+                                    error: "Internel server error " + deleteError
+                                })
+                            }
+                        })
+                    }else{
+                        callback(403, {
+                            error: "Authentication failed"
+                        })
+                    }
+                })     
+            }else{
+                callback(500, {
+                    error: "Internel server error " + idError
+                })
+            }
+        })
+    }else{
+        callback(400, {
+            error: "TokenError " + id
+        })
+    }
  }
  
  
